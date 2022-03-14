005c78c4cc1e4cc825924b34a564e54d5f89db5c
vamsi2004 <98633355+vamsi2004@users.noreply.github.com>
2022-03-14 21:44:28 +0530

    #include <stdio.h> int main() {   int n, reversed = 0, remainder, original;     scanf("%d", &n);     original = n;
        // reversed integer is stored in reversed variable
        while (n != 0) {
            remainder = n % 10;
            reversed = reversed * 10 + remainder;
            n /= 10;
        }
    
        // palindrome if orignal and reversed are equal
        if (original == reversed)
            printf("True");
        else
            printf("False");
    
        return 0;
    }

---
+++ Palindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+  int n, reversed = 0, remainder, original;
+    scanf("%d", &n);
+    original = n;
+
+    // reversed integer is stored in reversed variable
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignal and reversed are equal
+    if (original == reversed)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
+
+
